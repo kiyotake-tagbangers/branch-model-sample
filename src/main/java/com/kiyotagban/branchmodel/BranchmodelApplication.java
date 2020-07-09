33f6dc18c4c0f5e23a094a4b95dd417359db7abb
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-07-09 16:15:12 +0900

    init commit

---
+++ src/main/java/com/kiyotagban/branchmodel/BranchmodelApplication.java
@@ -0,0 +1,13 @@
+package com.kiyotagban.branchmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiyotagban/branchmodel/BranchmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyotagban.branchmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
